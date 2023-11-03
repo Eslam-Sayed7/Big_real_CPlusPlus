163333d13e707d9f40dafd28c76802977bba85e2
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2023-11-03 14:00:57 +0200

    Edit all bugs

--- BigReal.cpp
+++ BigReal.cpp
@@ -26,9 +26,8 @@ bool BigReal::isValidReal(string realNumber)
     }
     return false;
 }
+void BigReal::fill_zeros(BigReal&other) {
 
-void BigReal::fill_zeros(BigReal &other)
-{
     int maxSize = max(digits_d.size(), other.digits_d.size());
     int minSize = min(digits_d.size(), other.digits_d.size());
 
@@ -38,6 +37,7 @@ void BigReal::fill_zeros(BigReal &other)
         other.digits_d.insert(other.digits_d.begin(), numZeros, 0);
     else if (other.digits_d.size() > digits_d.size())
         digits_d.insert(digits_d.begin(), numZeros, 0);
+
 
     maxSize = max(digits_r.size(), other.digits_r.size());
     minSize = min(digits_r.size(), other.digits_r.size());
@@ -51,53 +51,43 @@ void BigReal::fill_zeros(BigReal &other)
         while (numZeros--)
             digits_r.push_back(0);
     }
-}
 
-void BigReal::ninesComplemet(BigReal &b)
-{
+}
+void BigReal::ninesComplemet(BigReal &b) {
     for (int i = 0; i < b.digits_r.size(); ++i) {
         b.digits_r[i] = 9 - b.digits_r[i];
     }
     for (int i = 0; i < b.digits_d.size(); ++i) {
         b.digits_d[i] = 9 - b.digits_d[i];
+
     }
 }
 
-BigReal BigReal::add(BigReal other)
-{
+BigReal BigReal::add(BigReal other) {
     int maxSize = max(digits_d.size(), other.digits_d.size());
     BigReal res;
+    res.digits_r.pop_back();
     int carry = 0;
-
     for (int i = 0; i < maxSize; ++i) {
         int sum = carry;
-
         if (i < digits_d.size())
             sum += digits_d[i];
         if (i < other.digits_d.size())
             sum += other.digits_d[i];
-        
         res.digits_d.push_back(sum % 10);
         carry = sum / 10;
     }
-    
     maxSize = max(digits_r.size(), other.digits_r.size());
-    
     for (int i = 0; i < maxSize; ++i) {
         int sum = carry;
-
         if (i < digits_r.size())
             sum += digits_r[i];
         if (i < other.digits_r.size())
             sum += other.digits_r[i];
-
         res.digits_r.push_back(sum % 10);
         carry = sum / 10;
     }
-    
-    if (carry != 0) 
-        res.digits_r.push_back(carry);
-    
+    if (carry != 0) res.digits_r.push_back(carry);
     return res;
 }
 
@@ -122,6 +112,21 @@ BigReal BigReal::compare_two_values(BigReal num1, BigReal num2)
             return num2;
     
     return num1;
+}
+
+void BigReal::remove_zeros()
+{
+    while(digits_r.size() >= 1 && digits_r.back() == 0)
+        digits_r.pop_back();
+    
+    reverse(digits_d.begin(), digits_d.end());
+
+    while(!digits_d.empty() && digits_d.back() == 0)
+        digits_d.pop_back();
+    
+    reverse(digits_d.begin(), digits_d.end());
+    if (digits_r.empty())
+        digits_r.push_back(0);
 }
 
 // Constructor Function
@@ -172,19 +177,11 @@ void BigReal::setNum(string realNumber)
             digits_d.push_back(digit);
         else
             digits_r.push_back(digit);
-    }
-
-    while(!digits_d.empty() && digits_d.back() == 0)
-        digits_d.pop_back();
-    
+    }    
     reverse(digits_d.begin(), digits_d.end());     
     reverse(digits_r.begin(), digits_r.end());     
-    
-    while(!digits_r.empty() && digits_r.back() == 0)
-        digits_r.pop_back();
 
-    if (digits_r.empty())
-        digits_r.push_back(0); 
+    remove_zeros();
 }
 
 int BigReal::SIZE()
@@ -196,7 +193,6 @@ int BigReal::SIZE()
 
 BigReal BigReal::operator+(BigReal &other) {
     BigReal res;
-
     fill_zeros(other);
 
     if (sign != other.sign) {
@@ -212,36 +208,35 @@ BigReal BigReal::operator+(BigReal &other) {
         } else {
             res.digits_r.resize(res.digits_r.size() - 1);
             BigReal tmp;
-            tmp.setNum(digits_d.empty() && other.digits_d.empty() ? "+1" : "+.1");
+            tmp.setNum(res.digits_d.empty() ? "+1" : "+.1");
             res = res.add(tmp);
             res.sign = '+';
         }
-        while (res.digits_r.back() == 0 && res.digits_r.size() > 1) {
-            res.digits_r.pop_back();
-        }
+
     } else {
         res = this->add(other);
         res.sign = sign;
     }
 
+    res.remove_zeros();
     return res;
 }
-
 BigReal BigReal::operator-(BigReal &other) {
     if (other.sign == '+')
         other.sign = '-';
     else
         other.sign = '+';
+        
     return *this + other;
 }
 
-BigReal BigReal::operator=(BigReal other)
-{
+
+BigReal BigReal::operator=(BigReal other) {
     if (this == &other)
         return *this;
-    this->digits_r.clear();    
+    this->digits_r.clear();
     this->digits_d.clear();
-    
+
     this->sign = other.sign;
 
     for (int i = 0; i < other.digits_r.size(); ++i)
@@ -252,7 +247,6 @@ BigReal BigReal::operator=(BigReal other)
 
     return *this;
 }
-
 // comparison operators (<, >, ==)
 
 bool BigReal::operator<(BigReal anotherReal)
@@ -311,10 +305,9 @@ bool BigReal::operator==(BigReal anotherReal)
 }
 
 // printing operator
-
 ostream &operator<<(ostream &out, BigReal num)
 {
-    if (num.digits_d.empty() && num.digits_r[0] == 0)
+    if (num.digits_d.empty() && num.digits_r.size() == 1 && num.digits_r[0] == 0)
     {
         out << 0;
         return out;
